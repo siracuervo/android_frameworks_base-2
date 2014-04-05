MoKee/android_frameworks_base

Multi-window support
Change-Id: I9b3d737ba7efa4d3677baec18edf3eb36d49ba14

core/java/android/content/Intent.java
@@ -3538,6 +3538,12 @@ public class Intent implements Parcelable, Cloneable {
      */
     public static final int FLAG_FLOATING_WINDOW = 0x00002000;
     /**
+     * If set in an Intent passed to {@link Context#startActivity Context.startActivity()},
+     * this flag will cause a newly launching task to be resized according to the split
+     * view metrics, making it running alongside another app.
+     */
+    public static final int FLAG_ACTIVITY_SPLIT_VIEW = 0x00001000;
+    /**
      * If set, when sending a broadcast only registered receivers will be
      * called -- no BroadcastReceiver components will be launched.
      */

siracuervo/android_frameworks_base-2

Change-Id: Ie5860c2105ad9c971157b41d65f8fab337461ef6
Conflicts:
	core/java/android/app/Activity.java
	core/java/android/inputmethodservice/InputMethodService.java
	packages/SystemUI/res/values/axxion_strings.xml
	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
	core/java/android/view/IWindowManager.aidl
	services/java/com/android/server/wm/WindowManagerService.java

core/java/android/content/Intent.java
@@ -3657,6 +3657,14 @@ public class Intent implements Parcelable, Cloneable {
      * in mutil window scenarios.
      */
     public static final int FLAG_FLOATING_WINDOW = 0x00002000;
+
+     /**
+	 * set in an Intent passed to {@link Context#startActivity Context.startActivity()},
+     * this flag will cause a newly launching task to be resized according to the split
+     * view metrics, making it running alongside another app.
+     */
+    public static final int FLAG_ACTIVITY_SPLIT_VIEW = 0x00002000;
+
     /**
      * If set, when sending a broadcast only registered receivers will be
      * called -- no BroadcastReceiver components will be launched.