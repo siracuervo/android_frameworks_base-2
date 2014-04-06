388ba2b65225667c27f1caa13cafa49f94cbec50
siracuervo <fl.siracusa@outlook.com.ar>
2014-04-06 17:08:06 -0300

    Revert "Revert "Revert "Runtime toggle of navbar"" so we ahve it again!"
    This reverts commit 2e5ff80e451c473f5d741dc746503488f255c3e8.

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10287,11 +10287,6 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
-    public boolean needsNavigationBar() {
-        return mPolicy.needsNavigationBar();
-    }
-
-    @Override
     public boolean hasMenuKeyEnabled() {
         return mPolicy.hasMenuKeyEnabled();
     }
